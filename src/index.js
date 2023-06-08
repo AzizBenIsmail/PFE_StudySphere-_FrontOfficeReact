c43cca3d9e17e1ad49e6b4fc007ff51602906ceb
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-06-08 10:55:19 +0100

    Intègre Backoffice

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss";
 import "assets/demo/demo.css";
+import "assets/scss/black-dashboard-react.scss";
 
 import Index from "views/Index.js";
 import LandingPage from "views/examples/LandingPage.js";
 import RegisterPage from "views/examples/RegisterPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
+import AdminLayout from "layouts/Admin/Admin.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -21,6 +23,7 @@ root.render(
       <Route path="/landing-page" element={<LandingPage />} />
       <Route path="/register-page" element={<RegisterPage />} />
       <Route path="/profile-page" element={<ProfilePage />} />
+      <Route path="/admin/*" element={<AdminLayout />} />
       <Route path="*" element={<Navigate to="/components" replace />} />
     </Routes>
   </BrowserRouter>
