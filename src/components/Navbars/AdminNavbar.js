55bc0c025c49457938feaa0a8b00f872f90bf219
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-07-13 15:40:22 +0100

    nettoyage de warning navbar Admin

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,190 +1,102 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from 'react'
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames'
 // reactstrap components
 import {
-  Button,
   Collapse,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  Input,
-  InputGroup,
-  NavbarBrand,
-  Navbar,
-  NavLink,
-  Nav,
   Container,
-  Modal,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
   NavbarToggler,
-  ModalHeader,
-} from "reactstrap";
-import { logout, getUserAuth } from "../../Service/apiUser";
-import Cookies from "js-cookie";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+  NavLink,
+  UncontrolledDropdown,
+} from 'reactstrap'
+import { getUserAuth, logout } from '../../Service/apiUser'
+import Cookies from 'js-cookie'
+import { useNavigate } from 'react-router-dom'
 
-function AdminNavbar(props) {
-  const [collapseOpen, setcollapseOpen] = React.useState(false);
-  const [modalSearch, setmodalSearch] = React.useState(false);
-  const [color, setcolor] = React.useState("navbar-transparent");
-  const [user, setUser] = useState([]);
-  const navigate = useNavigate();
-
-  React.useEffect(() => {
-    window.addEventListener("resize", updateColor);
-    // Specify how to clean up after this effect:
-    return function cleanup() {
-      window.removeEventListener("resize", updateColor);
-    };
-  });
-  // function that adds color white/transparent to the navbar on resize (this is for the collapse)
-  const updateColor = () => {
-    if (window.innerWidth < 993 && collapseOpen) {
-      setcolor("bg-white");
-    } else {
-      setcolor("navbar-transparent");
-    }
-  };
-  // this function opens and closes the collapse on small devices
-  const toggleCollapse = () => {
-    if (collapseOpen) {
-      setcolor("navbar-transparent");
-    } else {
-      setcolor("bg-white");
-    }
-    setcollapseOpen(!collapseOpen);
-  };
-  // this function is to open the Search modal
-  const toggleModalSearch = () => {
-    setmodalSearch(!modalSearch);
-  };
-  /////cookies
-  if (!Cookies.get("jwt_token")) {
-    window.location.replace("/login-page");
+function AdminNavbar (props) {
+  const [user, setUser] = useState([])
+  const navigate = useNavigate()
+/////cookies
+  if (!Cookies.get('jwt_token')) {
+    window.location.replace('/login-page')
   }
 
   //cookies
 
-  const jwt_token = Cookies.get("jwt_token");
+  const jwt_token = Cookies.get('jwt_token')
 
   const config = useMemo(() => {
     return {
       headers: {
         Authorization: `Bearer ${jwt_token}`,
       },
-    };
-  }, [jwt_token]);
+    }
+  }, [jwt_token])
 
   ////////
 
   const log = async () => {
     try {
       logout(config)
-        .then((res) => {
-          // console.log(res.data.user);
-          window.location.replace(`/login-page/`);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      .then(() => {
+        // console.log(res.data.user);
+        window.location.replace(`/login-page/`)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
       // console.log(res.status);
       // console.log("Valeur du cookie jwt_token :", jwt_token);
       // window.location.replace(`/login-page/`);
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
   const getAuthUser = async (config) => {
     await getUserAuth(config)
-      .then((res) => {
-        setUser(res.data.user);
-        // console.log(res.data.user);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    .then((res) => {
+      setUser(res.data.user)
+      // console.log(res.data.user);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
   useEffect(() => {
-    getAuthUser(config);
+    getAuthUser(config)
     const interval = setInterval(() => {
-      getAuthUser(config); // appel répété toutes les 10 secondes
-    }, 300000);
-    return () => clearInterval(interval); // nettoyage à la fin du cycle de vie du composant
-  }, [config]);
+      getAuthUser(config) // appel répété toutes les 10 secondes
+    }, 300000)
+    return () => clearInterval(interval) // nettoyage à la fin du cycle de vie du composant
+  }, [config])
   return (
     <>
-      <Navbar className={classNames("navbar-absolute", color)} expand="lg">
+      <Navbar className='navbar-absolute' expand="lg">
         <Container fluid>
           <div className="navbar-wrapper">
             <div
-              className={classNames("navbar-toggle d-inline", {
+              className={classNames('navbar-toggle d-inline', {
                 toggled: props.sidebarOpened,
               })}
             >
               <NavbarToggler onClick={props.toggleSidebar}>
-                <span className="navbar-toggler-bar bar1" />
-                <span className="navbar-toggler-bar bar2" />
-                <span className="navbar-toggler-bar bar3" />
+                <span className="navbar-toggler-bar bar1"/>
+                <span className="navbar-toggler-bar bar2"/>
+                <span className="navbar-toggler-bar bar3"/>
               </NavbarToggler>
             </div>
-            <NavbarBrand href="#pablo" onClick={(e) => e.preventDefault()}>
+            <NavbarBrand onClick={(e) => e.preventDefault()}>
               {props.brandText}
             </NavbarBrand>
           </div>
-          <NavbarToggler onClick={toggleCollapse}>
-            <span className="navbar-toggler-bar navbar-kebab" />
-            <span className="navbar-toggler-bar navbar-kebab" />
-            <span className="navbar-toggler-bar navbar-kebab" />
-          </NavbarToggler>
-          <Collapse navbar isOpen={collapseOpen}>
+          <Collapse navbar>
             <Nav className="ml-auto" navbar>
-              <InputGroup className="search-bar">
-                <Button color="link" onClick={toggleModalSearch}>
-                  <i className="tim-icons icon-zoom-split" />
-                  <span className="d-lg-none d-md-block">Search</span>
-                </Button>
-              </InputGroup>
-              <UncontrolledDropdown nav>
-                <DropdownToggle
-                  caret
-                  color="default"
-                  data-toggle="dropdown"
-                  nav
-                >
-                  <div className="notification d-none d-lg-block d-xl-block" />
-                  <i className="tim-icons icon-sound-wave" />
-                  <p className="d-lg-none">Notifications</p>
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-navbar" right tag="ul">
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      Mike John responded to your email
-                    </DropdownItem>
-                  </NavLink>
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      You have 5 more tasks
-                    </DropdownItem>
-                  </NavLink>
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      Your friend Michael is in town
-                    </DropdownItem>
-                  </NavLink>
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      Another notification
-                    </DropdownItem>
-                  </NavLink>
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      Another one
-                    </DropdownItem>
-                  </NavLink>
-                </DropdownMenu>
-              </UncontrolledDropdown>
               <UncontrolledDropdown nav>
                 <DropdownToggle
                   caret
@@ -196,17 +108,17 @@ function AdminNavbar(props) {
                     <img
                       alt="..."
                       src={`http://localhost:5000/images/${user.image_user}`}
-                      style={{ width: "35px", height: "35px" }}
+                      style={{ width: '35px', height: '35px' }}
                     />
                   </div>
-                  <b className="caret d-none d-lg-block d-xl-block" />
+                  <b className="caret d-none d-lg-block d-xl-block"/>
                 </DropdownToggle>
                 <DropdownMenu className="dropdown-navbar" right tag="ul">
                   <NavLink tag="li">
                     <DropdownItem
                       className="nav-item"
-                      onClick={(e) =>                               navigate(`/admin/UserDetails/${user._id}`)
-                    }
+                      onClick={() => navigate(`/admin/UserDetails/${user._id}`)
+                      }
                     >
                       Profile
                     </DropdownItem>
@@ -214,7 +126,7 @@ function AdminNavbar(props) {
                   <NavLink tag="li">
                     <DropdownItem className="nav-item">Settings</DropdownItem>
                   </NavLink>
-                  <DropdownItem divider tag="li" />
+                  <DropdownItem divider tag="li"/>
                   <NavLink tag="li">
                     <DropdownItem className="nav-item" onClick={() => log()}>
                       Log out
@@ -222,29 +134,13 @@ function AdminNavbar(props) {
                   </NavLink>
                 </DropdownMenu>
               </UncontrolledDropdown>
-              <li className="separator d-lg-none" />
+              <li className="separator d-lg-none"/>
             </Nav>
           </Collapse>
         </Container>
       </Navbar>
-      <Modal
-        modalClassName="modal-search"
-        isOpen={modalSearch}
-        toggle={toggleModalSearch}
-      >
-        <ModalHeader>
-          <Input placeholder="SEARCH" type="text" />
-          <button
-            aria-label="Close"
-            className="close"
-            onClick={toggleModalSearch}
-          >
-            <i className="tim-icons icon-simple-remove" />
-          </button>
-        </ModalHeader>
-      </Modal>
     </>
-  );
+  )
 }
 
-export default AdminNavbar;
+export default AdminNavbar
