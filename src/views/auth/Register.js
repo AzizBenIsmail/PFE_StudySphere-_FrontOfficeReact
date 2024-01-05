3d214d67a9698d8f7384ea53b39fb8ac9e064bf7
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-01-05 15:35:55 +0100

    préparation register

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,13 +1,78 @@
-import React from "react";
+import { React, useMemo } from 'react'
 import { Link } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import { getUserAuth ,register } from '../../Services/ApiUser'
+import { useState } from "react";
+import AddImage from "../../layouts/addImage";
 
-export default function Register() {
+export default function Register () {
+  const jwt_token = Cookies.get('jwt_token')
+
+  const config = useMemo(() => {
+    return {
+      headers: {
+        Authorization: `Bearer ${jwt_token}`,
+      },
+    }
+  }, [jwt_token])
+
+  //session
+  if (Cookies.get('jwt_token')) {
+    const fetchData = async () => {
+      try {
+        await getUserAuth(config).then((res) => {
+          if (res.data.user.userType === 'user') {
+            window.location.replace(`/landing/`)
+          }
+          if (res.data.user.userType === 'admin') {
+            window.location.replace(`/admin/`)
+          }
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }
+  const [image, setImage] = useState()
+  const [croppedImage, setCroppedImage] = useState(null)
+  const [User, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+    image_user: '',
+  })
+  const handleEvent = (croppedImageUrl, croppedImageBlob) => {
+    setCroppedImage(croppedImageUrl)
+    console.log(croppedImage)
+    setImage(croppedImageBlob)
+    console.log(croppedImageUrl)
+    console.log(croppedImageBlob)
+  }
+  const handlechange = (e) => {
+    setUser({ ...User, [e.target.name]: e.target.value })
+    console.log(User)
+  }
+  let formData = new FormData()
+  const add = async (e) => {
+    formData.append('username', User.username)
+    formData.append('email', User.email)
+    formData.append('password', User.password)
+    formData.append('image_user', image, `${User.username}+.png`)
+    const res = await register(formData)
+    .then(window.location.replace(`/login-page/`))
+    .catch((error) => {
+      console.log(error.response.data)
+    })
+    console.log(res.data)
+  }
   return (
     <>
       <div className="container mx-auto px-4 h-full">
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-6/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+            <div
+              className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
                   <h6 className="text-blueGray-500 text-sm font-bold">
@@ -22,7 +87,7 @@ export default function Register() {
                     <img
                       alt="..."
                       className="w-5 mr-1"
-                      src={require("assets/img/github.svg").default}
+                      src={require('assets/img/github.svg').default}
                     />
                     Github
                   </button>
@@ -33,12 +98,12 @@ export default function Register() {
                     <img
                       alt="..."
                       className="w-5 mr-1"
-                      src={require("assets/img/google.svg").default}
+                      src={require('assets/img/google.svg').default}
                     />
                     Google
                   </button>
                 </div>
-                <hr className="mt-6 border-b-1 border-blueGray-300" />
+                <hr className="mt-6 border-b-1 border-blueGray-300"/>
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                 <div className="text-blueGray-400 text-center mb-3 font-bold">
@@ -53,7 +118,7 @@ export default function Register() {
                       Name
                     </label>
                     <input
-                      type="email"
+                      type="text"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Name"
                     />
@@ -87,6 +152,34 @@ export default function Register() {
                     />
                   </div>
 
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Utilisateur
+                    </label>
+                    <input
+                      type="password"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Client ou Formateur "
+                    />
+                  </div>
+
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Photo De Profile
+                    </label>
+                    <input
+                      type="password"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Image"
+                    />
+                  </div>
+
                   <div>
                     <label className="inline-flex items-center cursor-pointer">
                       <input
@@ -95,7 +188,7 @@ export default function Register() {
                         className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
                       />
                       <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                        I agree with the{" "}
+                        I agree with the{' '}
                         <a
                           href="#pablo"
                           className="text-lightBlue-500"
@@ -138,5 +231,5 @@ export default function Register() {
         </div>
       </div>
     </>
-  );
+  )
 }
