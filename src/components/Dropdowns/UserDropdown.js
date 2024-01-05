90eb2d629051db353ba596e79088e51e73f5bd8c
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-01-05 13:18:39 +0100

    Update UserDropdown.js

--- src/components/Dropdowns/UserDropdown.js
+++ src/components/Dropdowns/UserDropdown.js
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from 'react'
 import { createPopper } from "@popperjs/core";
+import { getUserAuth, logout } from '../../Services/ApiUser'
+import Cookies from 'js-cookie'
+// import { useHistory } from 'react-router-dom';
 
 const UserDropdown = () => {
+  const [user, setUser] = useState([])
+
   // dropdown props
   const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
   const btnDropdownRef = React.createRef();
   const popoverDropdownRef = React.createRef();
+  // const history = useHistory();
+  //cookies
+  const jwt_token = Cookies.get('jwt_token')
+/////cookies
+  if (!Cookies.get('jwt_token')) {
+    window.location.replace('/login-page')
+  }
+
+  const config = useMemo(() => {
+    return {
+      headers: {
+        Authorization: `Bearer ${jwt_token}`,
+      },
+    }
+  }, [jwt_token])
+
+  ////////
+  useEffect(() => {
+    const getAuthUser = async (config) => {
+      await getUserAuth(config)
+      .then((res) => {
+        setUser(res.data.user)
+        // console.log(res.data.user);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+    getAuthUser(config)
+    const interval = setInterval(() => {
+      getAuthUser(config) // appel répété toutes les 10 secondes
+    }, 300000)
+    return () => clearInterval(interval) // nettoyage à la fin du cycle de vie du composant
+  }, [config])
+  const log = async () => {
+    try {
+      logout(config)
+      .then(() => {
+        // console.log(res.data.user);
+        window.location.replace(`/login/`)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const openDropdownPopover = () => {
+
     createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
       placement: "bottom-start",
     });
@@ -31,7 +85,7 @@ const UserDropdown = () => {
             <img
               alt="..."
               className="w-full rounded-full align-middle border-none shadow-lg"
-              src={require("assets/img/team-1-800x800.jpg").default}
+              src={`http://localhost:5000/images/${user.image_user}`}
             />
           </span>
         </div>
@@ -76,9 +130,8 @@ const UserDropdown = () => {
           className={
             "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
           }
-          onClick={(e) => e.preventDefault()}
-        >
-          Seprated link
+          onClick={() => log()}        >
+          Se déconnecter
         </a>
       </div>
     </>
