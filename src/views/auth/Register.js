92502691ea4622d653364c5ecf2184672f9dd0e5
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-01-10 16:59:48 +0100

    réglage problème email non unique

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,22 +1,20 @@
-import { React, useMemo ,useEffect  } from 'react'
-import { Link } from 'react-router-dom'
-import Cookies from 'js-cookie'
-import { getUserAuth ,register } from '../../Services/ApiUser'
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
-import { NotificationContainer, NotificationManager } from 'react-notifications'
+import { React, useMemo, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { getUserAuth, register } from '../../Services/ApiUser';
+import { useLocation } from 'react-router-dom';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
-export default function Register () {
-
-  const jwt_token = Cookies.get('jwt_token')
+export default function Register() {
+  const jwt_token = Cookies.get('jwt_token');
 
   const config = useMemo(() => {
     return {
       headers: {
         Authorization: `Bearer ${jwt_token}`,
       },
-    }
-  }, [jwt_token])
+    };
+  }, [jwt_token]);
 
   //session
   if (Cookies.get('jwt_token')) {
@@ -24,94 +22,116 @@ export default function Register () {
       try {
         await getUserAuth(config).then((res) => {
           if (res.data.user.role === 'client') {
-            window.location.replace(`/landing/`)
+            window.location.replace(`/landing/`);
           }
           if (res.data.user.role === 'admin') {
-            window.location.replace(`/admin/`)
+            window.location.replace(`/admin/`);
           }
-        })
+        });
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    fetchData()
+    };
+    fetchData();
   }
 
   const location = useLocation();
-  const message = new URLSearchParams(location.search).get("message");
-  const email = new URLSearchParams(location.search).get("email");
+  const message = new URLSearchParams(location.search).get('message');
+  const email = new URLSearchParams(location.search).get('email');
 
   const showNotification = (type, title, message) => {
     switch (type) {
       case 'success':
-        NotificationManager.success(title , message )
-        break
+        NotificationManager.success(title, message);
+        break;
       case 'error':
-        NotificationManager.error(title ,message )
-        break
+        NotificationManager.error(title, message);
+        break;
       case 'info':
-        NotificationManager.info(title , message);
+        NotificationManager.info(title, message);
         break;
       case 'warning':
-        NotificationManager.warning(title , message);
+        NotificationManager.warning(title, message);
         break;
-        default:
-        break
+      default:
+        break;
     }
-  }
+  };
+
   useEffect(() => {
-    showNotification('success', message, 'success')
+    showNotification('success', message, 'success');
 
-    const interval = setInterval(() => {
-    }, 1000000)
+    const interval = setInterval(() => {}, 1000000);
 
-    return () => clearInterval(interval)
-  }, [config,message])
-  // const [image, setImage] = useState()
+    return () => clearInterval(interval);
+  }, [config, message]);
+
   const [User, setUser] = useState({
-    // surnom: '',
     nom: '',
     prenom: '',
     email: email,
     password: '',
-    // role:'',
-    // image_user: '',
-  })
-  const handlechange = (e) => {
-    setUser({ ...User, [e.target.name]: e.target.value })
-    console.log(User)
-  }
-  // const handlechangeFile = (e) => {
-  //   setImage(e.target.files[0]);
-  //   console.log(e.target.files[0]);
-  // };
-  const [messageerr, setmessageerr] = useState();
-  let formData = new FormData()
-  const add = async (e) => {
-    formData.append('nom', User.nom)
-    formData.append('prenom', User.prenom)
-    formData.append('password', User.password)
-    formData.append('email', email)
-    // formData.append('role', User.role)
-    // formData.append('image_user', image, `${User.username}+.png`)
-    const res = await register(User);
-     console.log(res.data);
-    setmessageerr(res.data.message);
-    if (res.data.message === undefined) {
-      window.location.replace(`/auth/login/`);
-    }else{
-      showNotification('error', messageerr, 'Erreur')
+  });
+
+  const [passwordStrength, setPasswordStrength] = useState(0);
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setUser({ ...User, password: newPassword });
+
+    const strengthCode = checkPasswordStrength(newPassword);
+    setPasswordStrength(strengthCode);
+  };
+
+  const checkPasswordStrength = (password) => {
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasDigit = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+]/.test(password);
+
+    if (password.length < 8) {
+      return 1; // Faible
+    } else if (hasUppercase && hasLowercase && hasDigit && hasSpecialChar) {
+      return 3; // Fort
+    } else {
+      return 2; // Moyen
     }
-    // console.log(res.data)
-  }
+  };
+
+  const getStrengthColor = (strength) => {
+    switch (strength) {
+      case 1:
+        return 'bg-red-200';
+      case 2:
+        return 'bg-yellow-200';
+      case 3:
+        return 'bg-green-200';
+      default:
+        return '';
+    }
+  };
+
+  const add = async (e) => {
+    const formData = new FormData();
+    formData.append('nom', User.nom);
+    formData.append('prenom', User.prenom);
+    formData.append('password', User.password);
+    formData.append('email', email);
+    const res = await register(User);
+    console.log(res.data);
+    if (res.data.message === undefined) {
+      // window.location.replace(`/auth/login/`);
+    }else{
+      showNotification('error', res.data.message, 'Erreur')
+    }  };
+
   return (
     <>
-      <NotificationContainer/>
+      <NotificationContainer />
       <div className="container mx-auto px-4 h-full">
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-6/12 px-4">
-            <div
-              className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
                   <h6 className="text-blueGray-500 text-sm font-bold">
@@ -142,18 +162,15 @@ export default function Register () {
                     Google
                   </button>
                 </div>
-                <hr className="mt-6 border-b-1 border-blueGray-300"/>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                 <div className="text-blueGray-400 text-center mb-3 font-bold">
                   <small>Or sign up with credentials</small>
                 </div>
-                <form encType="multipart/form-data" >
+                <form encType="multipart/form-data">
                   <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
+                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
                       Nom
                     </label>
                     <input
@@ -161,25 +178,18 @@ export default function Register () {
                       placeholder="nom"
                       type="text"
                       name="nom"
-                      onChange={(e) => handlechange(e)}
+                      onChange={(e) => setUser({ ...User, nom: e.target.value })}
                       label="nom"
                       aria-label="nom"
-                      // defaultValue={email}
                     />
-                    {messageerr === "Le Nom doit contenir plus de 3 caractères" ||
-                    messageerr === "Le Nom doit contenir moins de 15 caractères" ? (
-                      <label style={{ color: "red" }}>
-                        Le Nom doit contenir plus de 3 et moin de 15
+                    {(User.nom.length < 3 || User.nom.length > 15) && (
+                      <label style={{ color: 'red' }}>
+                        Le Nom doit contenir entre 3 et 15 caractères.
                       </label>
-                    ) : (
-                      ""
                     )}
                   </div>
                   <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
+                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
                       Prenom
                     </label>
                     <input
@@ -187,67 +197,41 @@ export default function Register () {
                       placeholder="prenom"
                       type="text"
                       name="prenom"
-                      onChange={(e) => handlechange(e)}
+                      onChange={(e) => setUser({ ...User, prenom: e.target.value })}
                       label="prenom"
                       aria-label="prenom"
-                      // defaultValue={email}
                     />
-                    {messageerr === "Le Prenom doit contenir plus de 3 characters" ||
-                    messageerr === "Le Prenom doit contenir plus de 15 characters" ? (
-                      <label style={{ color: "red" }}>
-                        Le Prenom doit contenir plus de 3 et moin de 15
+                    {(User.prenom.length < 3 || User.prenom.length > 15) && (
+                      <label style={{ color: 'red' }}>
+                        Le Prenom doit contenir entre 3 et 15 caractères.
                       </label>
-                    ) : (
-                      ""
                     )}
                   </div>
                   <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
+                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
                       Password
                     </label>
                     <input
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      className={`border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 ${getStrengthColor(passwordStrength)}`}
                       placeholder="Password"
                       type="password"
                       name="password"
-                      onChange={(e) => handlechange(e)}
+                      onChange={(e) => handlePasswordChange(e)}
                       label="Password"
                       aria-label="Password"
                     />
-                    {messageerr === "Le Mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un symbole Exemple mdp : Exemple@123 | Exemple#123 | Exemple.123 | Exemple/123 | Exemple*123" ||
-                    messageerr === "Le Mot de passe doit contenir au moins 8 caractères" ? (
-                      <label style={{ color: "red" }}>
-                        Le Mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un symbole Exemple mdp : Exemple@123 | Exemple#123 | Exemple.123 | Exemple/123 | Exemple*123                      </label>
-                    ) : (
-                      ""
+                    {(passwordStrength === 1 || passwordStrength === 2) && (
+                      <label style={{ color: 'red' }}>
+                        Le Mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un symbole Exemple mdp : Exemple@123 | Exemple#123 | Exemple.123 | Exemple/123 | Exemple*123
+                      </label>
                     )}
-
+                    {passwordStrength === 1 && <label style={{ color: 'red' }}>Le Mot de passe doit contenir au moins 8 caractères</label>}
                     <div className="relative pt-1">
                       <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-red-200">
-                        <div style={{ width: '30%' }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"></div>
+                        <div style={{ width: `${(passwordStrength / 3) * 100}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"></div>
                       </div>
                     </div>
-
-
                   </div>
-
-                  {/*<div className="relative w-full mb-3">*/}
-                  {/*  <label*/}
-                  {/*    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"*/}
-                  {/*    htmlFor="grid-password"*/}
-                  {/*  >*/}
-                  {/*    Photo De Profile*/}
-                  {/*  </label>*/}
-                  {/*  <input*/}
-                  {/*    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"*/}
-                  {/*    placeholder="image_user"*/}
-                  {/*    name="image_user"*/}
-                  {/*    type="file"*/}
-                  {/*    onChange={(e) => handlechangeFile(e)}                    />*/}
-                  {/*</div>*/}
 
                   <div>
                     <label className="inline-flex items-center cursor-pointer">
@@ -283,11 +267,7 @@ export default function Register () {
             </div>
             <div className="flex flex-wrap mt-6 relative">
               <div className="w-1/2">
-                <a
-                  href="#pablo"
-                  // onClick={(e) => forget(User.email)}
-                  className="text-blueGray-200"
-                >
+                <a href="#pablo" className="text-blueGray-200">
                   <small> .</small>
                 </a>
               </div>
@@ -301,5 +281,5 @@ export default function Register () {
         </div>
       </div>
     </>
-  )
+  );
 }
