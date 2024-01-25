b8baab20b12dad744bb6bd323c84fbc6cbc7633c
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-01-25 12:41:45 +0100

    ordonner les routes

--- src/Services/ApiUser.js
+++ src/Services/ApiUser.js
@@ -1,29 +1,52 @@
-import axios from "axios";
+import axios from 'axios'
 
-const apiURL = "http://localhost:5000/auth";
+const apiURL = 'http://localhost:5000/auth'
 
-export async function register(user) {
-  return await axios.post(`${ apiURL }/inscrire`, user);
+export async function getUserAuth (config) {
+  return await axios.get(`${apiURL}/`, config)
 }
-export async function registerEmail(email) {
-  return await axios.post(`${ apiURL }/verification`, email);
+
+export async function registerEmail (email) {
+  return await axios.post(`${apiURL}/verification`, email)
 }
-export async function registerCentre(formData) {
-  return await axios.post(`${ apiURL }/inscrireCentre`, formData, {
-    headers: {"Content-Type": "multipart/form-data"}, withCredentials: true,
-  });
+
+export async function register (user) {
+  return await axios.post(`${apiURL}/inscrire`, user)
 }
+
+export async function registerCentre (formData) {
+  return await axios.post(`${apiURL}/inscrireCentre`, formData, {
+    headers: { 'Content-Type': 'multipart/form-data' }, withCredentials: true,
+  })
+}
+
+export async function LoginUser (user) {
+  return await axios.post(`${apiURL}/login`, user, {
+    withCredentials: true,
+  })
+}
+
+export async function forgetPassword (email) {
+  return await axios.put(`${apiURL}/forgetPassword`, { email })
+}
+
+export async function Password (user, config) {
+  return await axios.put(`${apiURL}/Password`, { user, config, withCredentials: true })
+}
+
+export async function logout (config) {
+  return await axios.get(`${apiURL}/logout`, {
+    config, withCredentials: true,
+  })
+}
+
 // export async function AddUserService(formData, config) {
 //   return await axios.post(`${ apiURL }/register`, formData, config, {
 //     headers: {"Content-Type": "multipart/form-data"}, withCredentials: true,
 //   });
 // }
 
-export async function LoginUser(user) {
-  return await axios.post(`${ apiURL }/login`, user, {
-    withCredentials: true,
-  });
-}
+
 
 // export async function getUsers(config) {
 //   return await axios.get(`${ apiURL }/AllUsers`, config);
@@ -49,16 +72,6 @@ export async function LoginUser(user) {
 //   return await axios.get(`${ apiURL }/searchUsers?term=${ term }`, config);
 // }
 
-export async function logout(config) {
-  return await axios.get(`${ apiURL }/logout`, {
-    config, withCredentials: true,
-  });
-}
-
-export async function getUserAuth(config) {
-  return await axios.get(`${ apiURL }/`, config);
-}
-
 // export async function getUserByID(id, config) {
 //   return await axios.get(`${ apiURL }/User/${ id }`, config);
 // }
@@ -72,26 +85,20 @@ export async function getUserAuth(config) {
 //   return await axios.delete(`${ apiURL }/${ id }`, config);
 // }
 
-export async function upgrade(id, config) {
-  return await axios.put(`${ apiURL }/upgrade`, {id}, config);
-}
+// export async function upgrade (id, config) {
+//   return await axios.put(`${apiURL}/upgrade`, { id }, config)
+// }
 
 // export async function downgrade(id, config) {
 //   return await axios.put(`${ apiURL }/downgrade`, {id}, config);
 // }
 
-export async function active(id, config) {
-  return await axios.put(`${ apiURL }/active`, {id}, config);
-}
+// export async function active (id, config) {
+//   return await axios.put(`${apiURL}/active`, { id }, config)
+// }
 
 // export async function desactive(id, config) {
 //   return await axios.put(`${ apiURL }/desactive`, {id}, config);
 // }
 
-export async function forgetPassword(email) {
-  return await axios.put(`${apiURL}/forgetPassword`, {email});
-}
 
-export async function Password(user) {
-  return await axios.put(`${apiURL}/Password`, {user});
-}
