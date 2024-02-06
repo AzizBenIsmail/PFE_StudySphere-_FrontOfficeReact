43b27c23cc735563226369360a348e10d265b087
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-06 13:48:06 +0100

    réglage désigne liste utilisateur

---
+++ src/views/admin/Ajouterutilisateur.js
@@ -0,0 +1,61 @@
+import React, { useMemo } from 'react'
+
+// components
+import Cookies from 'js-cookie'
+import { getUserAuth } from '../../Services/Apiauth'
+
+import CardLineChart from "components/Cards/CardLineChart.js";
+import CardBarChart from "components/Cards/CardBarChart.js";
+import CardPageVisits from "components/Cards/CardPageVisits.js";
+import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+
+export default function Dashboard() {
+  //cookies
+  const jwt_token = Cookies.get('jwt_token')
+
+  const config = useMemo(() => {
+    return {
+      headers: {
+        Authorization: `Bearer ${jwt_token}`,
+      },
+    }
+  }, [jwt_token])
+
+  //session
+  if (Cookies.get('jwt_token')) {
+    const fetchData = async () => {
+      try {
+        await getUserAuth(config).then((res) => {
+          if (res.data.user.role === 'client') {
+            window.location.replace(`/landing/`)
+          }
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  } else {
+    window.location.replace(`/`)
+  }
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardLineChart />
+        </div>
+        <div className="w-full xl:w-4/12 px-4">
+          <CardBarChart />
+        </div>
+      </div>
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardPageVisits />
+        </div>
+        <div className="w-full xl:w-4/12 px-4">
+          <CardSocialTraffic />
+        </div>
+      </div>
+    </>
+  );
+}
