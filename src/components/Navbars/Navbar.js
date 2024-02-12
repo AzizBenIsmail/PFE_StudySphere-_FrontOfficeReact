c4acec3a4578cc4551fac8be989232c0d2b60ae1
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-12 14:46:31 +0100

    personnalisions navbar

--- src/components/Navbars/Navbar.js
+++ src/components/Navbars/Navbar.js
@@ -1,21 +1,24 @@
 /*eslint-disable*/
-import React, { useEffect, useMemo, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useMemo, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 // components
-import { SlLogin } from 'react-icons/sl'
-import { GiBurningDot } from 'react-icons/gi'
-import Cookies from 'js-cookie'
-import { getUserAuth, logout } from '../../Services/Apiauth'
+import Cookies from "js-cookie";
+import { getUserAuth, logout } from "../../Services/Apiauth";
+import UserDropdown from "../Dropdowns/UserDropdownLanding";
+import { FaSchool } from "react-icons/fa";
+import { MdCastForEducation } from "react-icons/md";
 
-export default function Navbar (props) {
-  const [user, setUser] = useState([])
-  const [navbarOpen, setNavbarOpen] = React.useState(false)
+export default function Navbar(props) {
+  const [user, setUser] = useState([]);
+  const history = useHistory();
+
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
   //cookies
-  const jwt_token = Cookies.get('jwt_token')
-/////cookies
-  if (!Cookies.get('jwt_token')) {
-    window.location.replace('/login-page')
+  const jwt_token = Cookies.get("jwt_token");
+  /////cookies
+  if (!Cookies.get("jwt_token")) {
+    window.location.replace("/login-page");
   }
 
   const config = useMemo(() => {
@@ -23,65 +26,52 @@ export default function Navbar (props) {
       headers: {
         Authorization: `Bearer ${jwt_token}`,
       },
-    }
-  }, [jwt_token])
+    };
+  }, [jwt_token]);
 
   ////////
   useEffect(() => {
     const getAuthUser = async (config) => {
       await getUserAuth(config)
-      .then((res) => {
-        setUser(res.data.user)
-        // console.log(res.data.user);
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
-    getAuthUser(config)
+        .then((res) => {
+          setUser(res.data.user);
+          // console.log(res.data.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getAuthUser(config);
     const interval = setInterval(() => {
-      getAuthUser(config) // appel répété toutes les 10 secondes
-    }, 300000)
-    return () => clearInterval(interval) // nettoyage à la fin du cycle de vie du composant
-  }, [config])
-  const log = async (config,user) => {
+      getAuthUser(config); // appel répété toutes les 10 secondes
+    }, 300000);
+    return () => clearInterval(interval); // nettoyage à la fin du cycle de vie du composant
+  }, [config]);
+  const log = async (config, user) => {
     try {
-      logout(config,user._id)
-      .then(() => {
-        // console.log(res.data.user);
-        window.location.replace(`/login/`)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+      logout(config, user._id)
+        .then(() => {
+          // console.log(res.data.user);
+          window.location.replace(`/login/`);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   return (
     <>
-      <nav
-        className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
+      <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div
-            className="w-full relative flex justify-between items-center lg:w-auto lg:static lg:block lg:justify-start">
-            <div
-              className="text-white text-sm font-bold leading-relaxed inline-flex items-center mr-4 py-2 whitespace-nowrap uppercase"
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link
+              className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              to="/landing"
             >
-              <img
-                src={require('assets/img/Logo.png').default}
-                alt="..."
-                className="mr-4"
-                style={{ maxWidth: '20%', height: '10%' }}
-              />
-              <GiBurningDot size={25} className="mr-4"/>
-              <img
-                src={require('assets/img/LogoBridge.png').default}
-                alt="..."
-                className="mr-4"
-                style={{ maxWidth: '20%', height: '10%' }}
-              />
-            </div>
+              StudySphere
+            </Link>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -90,40 +80,77 @@ export default function Navbar (props) {
               <i className="text-white fas fa-bars"></i>
             </button>
           </div>
-
           <div
             className={
-              'lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none' +
-              (navbarOpen ? ' block rounded shadow-lg' : ' hidden')
+              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
+              (navbarOpen ? " block rounded shadow-lg" : " hidden")
             }
             id="example-navbar-warning"
           >
             <ul className="flex flex-col lg:flex-row list-none mr-auto">
               <li className="flex items-center">
-                <a
-                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus?ref=nr-auth-navbar"
+                <button
+                  className="lg:text-white lg:hover:text-indigo-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  onClick={() => history.push("/")}
                 >
-                </a>
+                  <MdCastForEducation
+                    className="mr-2"
+                    style={{ fontSize: "24px" }}
+                  />
+                  Formation
+                </button>
+              </li>
+              <li className="flex items-center ml-2">
+                <button
+                  className="lg:text-white lg:hover:text-lightBlue-800 text-blueGray-600 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  onClick={() => history.push("/")}
+                >
+                  <FaSchool className="mr-2" style={{ fontSize: "24px" }} />
+                  Centre de formation
+                </button>
               </li>
             </ul>
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-
+              <li className="flex items-center">{/*<PagesDropdown />*/}</li>
               <li className="flex items-center">
-                <Link
-                  className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150 flex items-center"
-                  type="button"
-                  onClick={() => log(config,user)}
+                <a
+                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-react%2F%23%2F"
+                  target="_blank"
                 >
-                  <SlLogin className="mr-2"/> {/* Ajoutez cette ligne pour l'icône */}
-                  deconnnecter
-                </Link>
+                  <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Share</span>
+                </a>
               </li>
 
+              <li className="flex items-center">
+                <a
+                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-react%2F%23%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20React%20UI%20Kit%20and%20Admin.%20Let%20Notus%20React%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level.%20"
+                  target="_blank"
+                >
+                  <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-twitter text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Tweet</span>
+                </a>
+              </li>
+
+              <li className="flex items-center">
+                <a
+                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="https://github.com/creativetimofficial/notus-react?ref=nr-auth-navbar"
+                  target="_blank"
+                >
+                  <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-github text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Star</span>
+                </a>
+              </li>
+              <li className="flex items-center">
+                <UserDropdown />
+              </li>
             </ul>
           </div>
         </div>
       </nav>
     </>
-  )
+  );
 }
