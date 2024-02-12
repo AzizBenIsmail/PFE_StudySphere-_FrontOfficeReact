076658538d2e82ce53c3a02a63d0c8aeb5678722
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-12 15:45:01 +0100

    modifier profile cote client

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,12 +1,16 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Cookies from 'js-cookie'
 
 import Navbar from "components/Navbars/Navbar.js";
 import Footer from "components/Footers/Footer.js";
 import { getUserAuth } from '../Services/Apiauth'
+import { useHistory, useParams } from 'react-router-dom'
+import { FaUserCog } from 'react-icons/fa'
+import { getUserByID } from '../Services/ApiUser'
 
 export default function Profile() {
   const jwt_token = Cookies.get('jwt_token')
+  const history = useHistory();
 
   const config = useMemo(() => {
     return {
@@ -33,6 +37,28 @@ export default function Profile() {
   }else{
     window.location.replace(`/`)
   }
+
+  const param = useParams()
+
+  const [User, setUser] = useState({})
+
+  useEffect(() => {
+
+    const getUser = async (config) => {
+      await getUserAuth(config).then((res) => {
+        setUser(res.data.user)
+        console.log(res.data.user)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+
+    getUser(config)
+
+    const interval = setInterval(() => {}, 1000000)
+
+    return () => clearInterval(interval)
+  }, [config, param.id])
 
   return (
     <>
@@ -90,9 +116,23 @@ export default function Profile() {
                       <button
                         className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                         type="button"
+                        onClick={() =>
+                          history.push(`/edit/${User._id}?u=${User.role.toLowerCase()}`)
+                        }
                       >
-                        Connect
+                        Edit
                       </button>
+                      {/*<button*/}
+                      {/*  className="text-sm py-2 px-4 font-normal block w-full flex items-center justify-start bg-transparent text-white"*/}
+                      {/*  type="button"*/}
+
+                      {/*>*/}
+                      {/*  <FaUserCog*/}
+                      {/*    className="mr-2"*/}
+                      {/*    style={{ fontSize: '20px' }}*/}
+                      {/*  />*/}
+                      {/*  Edit*/}
+                      {/*</button>*/}
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
