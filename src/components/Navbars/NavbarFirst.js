83fb6851da75c5b9807c36caf12f41312af42dcd
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-14 00:22:20 +0100

    Ajouter Quit Button

--- src/components/Navbars/NavbarFirst.js
+++ src/components/Navbars/NavbarFirst.js
@@ -83,15 +83,15 @@ export default function Navbar (props) {
               {/*  </a>*/}
               {/*</li>*/}
 
-              {/*  <li className="flex items-center">*/}
-              {/*    <Link*/}
-              {/*      className="text-lightBlue-500 bg-transparent border border-solid border-lightBlue-500 hover:bg-lightBlue-500 hover:text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"*/}
-              {/*      type="button"*/}
-              {/*      to="/auth/registerCentre"*/}
-              {/*    >*/}
-              {/*      Rejoignez-nous en tant que Centre de Formation*/}
-              {/*    </Link>*/}
-              {/*  </li>*/}
+                <li className="flex items-center">
+                  <Link
+                    className="text-lightBlue-500 bg-transparent border border-solid border-lightBlue-500 hover:bg-lightBlue-500 hover:text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    to="/landing"
+                  >
+                    Quitter
+                  </Link>
+                </li>
             </ul>
           </div>
         </div>
