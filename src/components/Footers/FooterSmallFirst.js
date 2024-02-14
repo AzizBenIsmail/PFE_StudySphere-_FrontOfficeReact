d6186340499b7f368ded3e3a629500cd47ebdd2c
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-14 10:49:27 +0100

    réglage mise en page

--- src/components/Footers/FooterSmallFirst.js
+++ src/components/Footers/FooterSmallFirst.js
@@ -10,12 +10,12 @@ export default function FooterSmall(props) {
             : "relative") + " pb-6"
         }
       >
-        <div className="container mx-auto px-4">
+        <div className="container mx-auto px-4 ">
           <hr className="mb-6 border-b-1 border-blueGray-600" />
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4">
               <div className="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
-                Copyright © {new Date().getFullYear()}{" "}
+                Copyright © {new Date().getFullYear()}
                 <a
                   href="https://www.creative-tim.com?ref=nr-footer-small"
                   className="text-white hover:text-blueGray-300 text-sm font-semibold py-1"
--- src/layouts/First.js
+++ src/layouts/First.js
@@ -1,17 +1,14 @@
 import { React, Suspense, useMemo } from 'react'
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Redirect, Route, Switch } from 'react-router-dom'
 
 // components
-
 // import Navbar from "components/Navbars/AuthNavbar.js";
-import FooterSmall from "components/Footers/FooterSmallFirst.js";
-import Navbar from "../components/Navbars/NavbarFirst";
+import FooterSmall from 'components/Footers/FooterSmallFirst.js'
+import Navbar from '../components/Navbars/NavbarFirst'
 
 // views
-
 // import Login from "views/auth/Login.js";
-import FirstStep from "views/FirstStep/FirstStep.js";
-
+import FirstStep from 'views/FirstStep/FirstStep.js'
 
 import { InfinitySpin } from 'react-loader-spinner'
 import Cookies from 'js-cookie'
@@ -19,7 +16,7 @@ import { getUserAuth } from '../Services/Apiauth'
 // import Cookies from 'js-cookie'
 // import { getUserAuth } from '../Services/Apiauth'
 
-export default function First() {
+export default function First () {
   const jwt_token = Cookies.get('jwt_token')
 
   const config = useMemo(() => {
@@ -44,30 +41,31 @@ export default function First() {
       }
     }
     fetchData()
-  }else{
+  } else {
     window.location.replace(`/`)
   }
   return (
     <>
-      <Navbar transparent />
+      <Navbar transparent/>
       <main>
-        <section className="relative w-full h-full py-42 min-h-screen">
+        <section className="relative w-full h-full py-30 min-h-screen">
           <div
             className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
             style={{
               backgroundImage:
-                "url(" + require("assets/img/FirstStep.png").default + ")",
+                'url(' + require('assets/img/FirstStep.png').default + ')',
             }}
           ></div>
-          <Suspense fallback={<InfinitySpin width="200" height="200" color="#4fa94d" />}>
+          <Suspense fallback={<InfinitySpin width="200" height="200" color="#4fa94d"/>}>
             <Switch>
-              <Route path="/First/Step" exact component={FirstStep} />
-              <Redirect from="/auth" to="/First/Step" />
+              <Route path="/First/Step" exact component={FirstStep}/>
+              <Redirect from="/auth" to="/First/Step"/>
             </Switch>
           </Suspense>
-          <FooterSmall absolute />
+
         </section>
+        <FooterSmall absolute/>
       </main>
     </>
-  );
+  )
 }
