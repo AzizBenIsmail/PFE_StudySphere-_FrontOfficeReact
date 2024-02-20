bc9bcafc223584900ea8b1b19eb7f0ee2b5a90c5
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-21 00:29:36 +0100

    personnalisation profile

--- src/components/Dropdowns/UserDropdownLanding.js
+++ src/components/Dropdowns/UserDropdownLanding.js
@@ -123,9 +123,9 @@ const UserDropdown = () => {
           className={
             "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
           }
-          onClick={(e) => e.preventDefault()}
+          onClick={() => history.push("/First/Step")}
         >
-          Another action
+          Paramètre
         </button>
         <button
           className={
