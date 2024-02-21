e7aa26cf6a5b0ab04679af0a12038e19a43e15f8
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-21 23:16:59 +0100

    ajouter addPreferencesCentre et getPreferences

--- src/Services/ApiPref.js
+++ src/Services/ApiPref.js
@@ -4,11 +4,13 @@ const apiURL = 'http://localhost:5000/pref'
 
 
 export async function addPreferences(id, preferencesData, config) {
-  try {
-    const response = await axios.put(`${apiURL}/addPref/${id}`, preferencesData, config);
-    return response.data;
-  } catch (error) {
-    console.error('Error adding preferences:', error);
-    throw error;
-  }
+    return await axios.put(`${apiURL}/addPref/${id}`, preferencesData, config);
+}
+
+export async function addPreferencesCentre(id, preferencesData, config) {
+  return await axios.put(`${apiURL}/addPrefCentre/${id}`, preferencesData, config);
+}
+
+export async function getPreferences(id,config) {
+  return await axios.get(`${ apiURL }/GetPref/:id`, config);
 }
