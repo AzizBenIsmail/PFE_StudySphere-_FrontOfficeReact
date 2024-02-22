68b85dce400e1c18570bae754179693b178f3ac4
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-22 17:02:00 +0100

    consommation api update préférence

--- src/Services/ApiPref.js
+++ src/Services/ApiPref.js
@@ -11,5 +11,13 @@ export async function addPreferencesCentre(id, preferencesData, config) {
 }
 
 export async function getPreferences(id,config) {
-  return await axios.get(`${ apiURL }/GetPref/:id`, config);
+  return await axios.get(`${ apiURL }/GetPref/${id}`, config);
+}
+
+export async function updatePrefClient(id,preferencesData,config) {
+  return await axios.put(`${ apiURL }/updatePreferences/${id}`,preferencesData, config);
+}
+
+export async function updatePrefCenter(id,preferencesData,config) {
+  return await axios.put(`${ apiURL }/UpdatePreferencesCentre/${id}`,preferencesData, config);
 }
