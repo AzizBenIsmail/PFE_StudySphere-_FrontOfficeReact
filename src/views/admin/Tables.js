eed59212fe1442c185bd88e53186a7a93d505080
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-26 13:29:56 +0100

    réglage import

--- src/views/admin/Tables.js
+++ src/views/admin/Tables.js
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react'
 
 // components
 
-import ListUsers from "../admin/ListUsers";
+import ListUsers from "./users/ListUsers";
 // import CardTable from "../../components/Cards/CardTable";
 import Cookies from 'js-cookie'
 import { getUserAuth } from '../../Services/Apiauth'
