25d04b3814cdd1d1cdfee9c9a879ca6c188a0d29
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-29 16:43:46 +0100

    modification du designe pour le registre

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -190,53 +190,11 @@ export default function Register () {
             <div
               className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <Link
-                    className="text-white text-sm font-bold leading-relaxed inline-flex items-center mr-4 py-2 whitespace-nowrap uppercase"
-                    to="/"
-                  >
-                    <img
-                      src={require('assets/img/LogoDark.png').default}
-                      alt="..."
-                      className="mr-4"
-                      style={{ maxWidth: '40%', height: '20%' }}
-                    />
-                    <GiBurningDot size={45} className="mr-4" style={{ color: 'red' }}/>
-                    <img
-                      src={require('assets/img/LogoBridge.png').default}
-                      alt="..."
-                      className="mr-4"
-                      style={{ maxWidth: '40%', height: '20%' }}
-                    />
-                  </Link>
-                  <h6 className="text-blueGray-500 text-sm font-bold">
+                <div className="text-center ">
+                  <h6 className="mt-6 text-3xl font-semibold leading-normal mt-0 mb-2 text-lightBlue-800">
                     S'inscrire
                   </h6>
                 </div>
-                {/*  <div className="btn-wrapper text-center">*/}
-                {/*    <button*/}
-                {/*      className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"*/}
-                {/*      type="button"*/}
-                {/*    >*/}
-                {/*      <img*/}
-                {/*        alt="..."*/}
-                {/*        className="w-5 mr-1"*/}
-                {/*        src={require('assets/img/github.svg').default}*/}
-                {/*      />*/}
-                {/*      Github*/}
-                {/*    </button>*/}
-                {/*    <button*/}
-                {/*      className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"*/}
-                {/*      type="button"*/}
-                {/*    >*/}
-                {/*      <img*/}
-                {/*        alt="..."*/}
-                {/*        className="w-5 mr-1"*/}
-                {/*        src={require('assets/img/google.svg').default}*/}
-                {/*      />*/}
-                {/*      Google*/}
-                {/*    </button>*/}
-                {/*  </div>*/}
                 <hr className="mt-6 border-b-1 border-blueGray-300"/>
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
@@ -319,25 +277,25 @@ export default function Register () {
                     </div>
                   </div>
 
-                  <div>
-                    <label className="inline-flex items-center cursor-pointer">
-                      <input
-                        id="customCheckLogin"
-                        type="checkbox"
-                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                      />
-                      <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                        I agree with the{' '}
-                        <a
-                          href="#pablo"
-                          className="text-lightBlue-500"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          Privacy Policy
-                        </a>
-                      </span>
-                    </label>
-                  </div>
+                  {/*<div>*/}
+                  {/*  <label className="inline-flex items-center cursor-pointer">*/}
+                  {/*    <input*/}
+                  {/*      id="customCheckLogin"*/}
+                  {/*      type="checkbox"*/}
+                  {/*      className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"*/}
+                  {/*    />*/}
+                  {/*    <span className="ml-2 text-sm font-semibold text-blueGray-600">*/}
+                  {/*      I agree with the{' '}*/}
+                  {/*      <a*/}
+                  {/*        href="#pablo"*/}
+                  {/*        className="text-lightBlue-500"*/}
+                  {/*        onClick={(e) => e.preventDefault()}*/}
+                  {/*      >*/}
+                  {/*        Privacy Policy*/}
+                  {/*      </a>*/}
+                  {/*    </span>*/}
+                  {/*  </label>*/}
+                  {/*</div>*/}
 
                   <div className="text-center mt-6">
                     <button
