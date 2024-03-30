ce87462c245b8612ab15d702c6ff393cf3590e9b
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-30 23:07:18 +0100

    interface setting 80%

--- src/views/FrontOffice/client/Setting/Setting.js
+++ src/views/FrontOffice/client/Setting/Setting.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import Cookies from 'js-cookie'
 
-import Navbar from "../../../../components/Navbars/Navbar.js";
-import Footer from "../../../../components/Footers/FooterSmall.js";
+import Navbar from '../../../../components/Navbars/Navbar.js'
+import Footer from '../../../../components/Footers/FooterSmall.js'
 import { getUserAuth } from '../../../../Services/Apiauth'
 import { useHistory, useParams } from 'react-router-dom'
-  // import { FaUserCog } from 'react-icons/fa'
-  // import { getUserByID } from '../Services/ApiUser'
-import { FaRegUserCircle } from "react-icons/fa";
+// import { FaUserCog } from 'react-icons/fa'
+// import { getUserByID } from '../Services/ApiUser'
+import { FaRegUserCircle } from 'react-icons/fa'
+import { MdOutlineSecurity, MdRoomPreferences } from 'react-icons/md'
+import { IoIosCard, IoMdNotificationsOutline } from 'react-icons/io'
+import { BiSolidUserAccount } from 'react-icons/bi'
 
-
-export default function Profile() {
+export default function Profile () {
   const jwt_token = Cookies.get('jwt_token')
-  const history = useHistory();
+  const history = useHistory()
 
   const config = useMemo(() => {
     return {
@@ -36,7 +38,7 @@ export default function Profile() {
       }
     }
     fetchData()
-  }else{
+  } else {
     window.location.replace(`/`)
   }
 
@@ -88,11 +90,28 @@ export default function Profile() {
           </div>
           <div className="container mx-auto flex justify-center">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
-              <div className="px-6 flex py-3"> {/* Changé items-center en items-start */}
-                <FaRegUserCircle  style={{ fontSize: '50px' }} className="mt-3" /> {/* Ajouté style pour définir la taille de l'icône */}
-                <div className="ml-4 leading-parametre mt-2"> {/* Ajouté une marge à gauche pour séparer l'icône du texte */}
-                  <h3 className="text-2xl font-semibold  text-blueGray-700">
+              <div className="px-6 flex py-3" onClick={() => {
+                if (User.role === 'client') {
+                  console.log(User)
+                  console.log(User.preferences)
+                  if (User.preferences === undefined) {
+                    history.push("/First");
+                  } else {
+                    history.push("/First/UpdatePreferences");
+                  }
+                }
+
+                if (User.role === 'centre') {
+                  if (!User.xp) {
+                    history.push("/First");
+                  }
+                }
+              }}>
+                <FaRegUserCircle style={{ fontSize: '50px' }} className="mt-3" />
+                <div className="ml-4 leading-parametre mt-2">
+                  <h3 className="text-2xl font-semibold text-blueGray-700">
                     Informations personnelles
+                    <span className="text-blue-500 text-xs ml-1"> (cliquable)</span> {/* Indication de clicabilité */}
                   </h3>
                   <h3 className="text-1xl font-normal text-blueGray-600">
                     Informations personnelles
@@ -100,33 +119,50 @@ export default function Profile() {
                 </div>
               </div>
             </div>
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
-              <div className="px-6">
-                <div className="text-center">
-                  <h3 className="text-4xl font-semibold leading-parametre mb-2 text-blueGray-700 mb-2">
+
+            <div
+              className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4"
+              >
+              <div className="px-6 flex py-3">
+                <MdRoomPreferences style={{ fontSize: '50px' }} className="mt-3"/>
+                <div className=" ml-4 leading-parametre mt-2">
+                  <h3 className="text-2xl font-semibold text-blueGray-700 ">
                     Préférences
                   </h3>
+                  <h3 className="text-1xl font-normal mb-2 text-blueGray-600 ">
+                    Modifiez votre Preference, votre devise et vos exigences en matière d'accessibilité.
+                  </h3>
                 </div>
               </div>
             </div>
           </div>
         </section>
         <section className="relative py-1 bg-blueGray-200">
-          <div className="container mx-auto  flex justify-center">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
-              <div className="px-6">
-                <div className="text-center">
-                  <h3 className="text-4xl font-semibold leading-parametre mb-2 text-blueGray-700 mb-2">
+          <div className="container mx-auto flex justify-center">
+            <div
+              className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
+              <div className="px-6 flex py-3">
+                <MdOutlineSecurity style={{ fontSize: '50px' }} className="mt-3"/>
+                <div className="ml-4 leading-parametre mt-2">
+                  <h3 className="text-2xl font-semibold text-blueGray-700">
                     Sécurité
                   </h3>
+                  <h3 className="text-1xl font-normal mb-2 text-blueGray-600">
+                    Modifiez vos paramètres de sécurité, configurez une authentification sécurisée ou supprimez votre
+                    compte </h3>
                 </div>
               </div>
             </div>
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
-              <div className="px-6">
-                <div className="text-center">
-                  <h3 className="text-4xl font-semibold leading-parametre mb-2 text-blueGray-700 mb-2">
+            <div
+              className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
+              <div className="px-6 flex py-6">
+                <IoMdNotificationsOutline style={{ fontSize: '50px' }} className="mt-3"/>
+                <div className="ml-4 leading-parametre mt-2 ">
+                  <h3 className="text-2xl font-semibold text-blueGray-700 ">
                     Notifications par e-mail
+                  </h3>
+                  <h3 className="text-1xl font-normal text-blueGray-600">
+                    Choisisser les notification que vous recevrez et desabonner-bous des autres.
                   </h3>
                 </div>
               </div>
@@ -135,29 +171,41 @@ export default function Profile() {
         </section>
         <section className="relative py-1 bg-blueGray-200">
           <div className="container mx-auto  flex justify-center">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
-              <div className="px-6">
-                <div className="text-center">
-                  <h3 className="text-4xl font-semibold leading-parametre mb-2 text-blueGray-700 mb-2">
-                    Prefrence
+            <div
+              className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
+              <div className="px-6 flex py-6">
+                <IoIosCard style={{ fontSize: '50px' }} className="mt-3"/>
+                <div className="ml-4 leading-parametre mt-2">
+                  <h3 className="text-2xl font-semibold text-blueGray-700 ">
+                    Information de paiement
+                  </h3>
+                  <h3 className="text-1xl font-normal mb-2 text-blueGray-600 ">
+                    Ajouter ou Supprimer vos moyens de paiment en toute securite
                   </h3>
                 </div>
               </div>
             </div>
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
-              <div className="px-6">
-                <div className="text-center">
-                  <h3 className="text-4xl font-semibold leading-parametre mb-2 text-blueGray-700 mb-2">
-                    Prefrence
+            <div
+              className="relative flex flex-col min-w-0 break-words bg-white w-1/2 mb-2 mr-2 shadow-xl rounded-lg -mt-4">
+              <div className="px-6 flex py-6">
+                <BiSolidUserAccount style={{ fontSize: '50px' }} className="mt-3"/>
+                <div className="ml-4 leading-parametre mt-2">
+                  <h3 className="text-2xl font-semibold mb-2 text-blueGray-700 ">
+                    Confientialiter
+                  </h3>
+                  <h3 className="text-1xl font-normal mb-2 text-blueGray-600">
+                    Exercez vos droits relatifs a la protection de la confidentialite et controler la maniere dont vos
+                    donnees sont utilisees
                   </h3>
                 </div>
               </div>
             </div>
           </div>
         </section>
-        <Footer absolute />
+        {/*<Footer absolute />*/}
+        <Footer/>
       </main>
     </>
 
-  );
+  )
 }
