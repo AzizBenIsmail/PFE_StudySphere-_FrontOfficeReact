e21457e5269d3490b3131748213970ebcd35de49
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-31 16:02:02 +0100

    adapter update centre

--- src/views/FrontOffice/client/updateProfile.js
+++ src/views/FrontOffice/client/updateProfile.js
@@ -19,6 +19,11 @@ export default function Dashboard() {
   //cookies
   const jwt_token = Cookies.get("jwt_token");
   const history = useHistory();
+  const location = useLocation();
+  const message = new URLSearchParams(location.search).get("u");
+  const [User, setUser] = useState({
+    role: message ,
+  });
 
   const config = useMemo(() => {
     return {
@@ -54,20 +59,12 @@ export default function Dashboard() {
 
     fetchData();
   }, [history, jwt_token]); // Inclure history et jwt_token dans le tableau de dépendances
-  const location = useLocation();
-  const message = new URLSearchParams(location.search).get("u");
+
 
 
   const [n, setN] = useState(0); // Ajout de la variable n
   const [emailError, setEmailError] = useState("");
   const [image, setImage] = useState()
-
-  const [User, setUser] = useState({
-    role: message ,
-  });
-
-
-
 
   const handlechange = (e) => {
     setUser({ ...User, [e.target.name]: e.target.value });
@@ -119,7 +116,7 @@ export default function Dashboard() {
       formData.append("nom", User.nom);
       formData.append("prenom", User.prenom);
       if (image !== undefined) {
-        formData.append("image_user", image, `${User.nom}+.png`);
+        formData.append("image_user", image, `${User.nom}.png`);
       }
         const res = await updateUser(formData, User._id, config);
         console.log(res.data);
@@ -132,45 +129,28 @@ export default function Dashboard() {
   };
 
   const addCentre = async (e) => {
-    const normalizedNom = User.nom.toLowerCase();
-    const passwordLowerCase = User.password.toLowerCase();
-    if (emailError === "Veuillez entrer une adresse e-mail valide.") {
-    } else if (User.nom === "" && User.email === "" && User.password === "") {
-      setN(4); // Utilisation de setN pour mettre à jour la valeur de n
-    } else if (User.nom === "" && User.email === "") {
-      setN(5); // Utilisation de setN pour mettre à jour la valeur de n
-    } else if (User.email === "" && User.password === "") {
-      setN(6); // Utilisation de setN pour mettre à jour la valeur de n
-    } else if (User.nom === "" && User.password === "") {
-      setN(7); // Utilisation de setN pour mettre à jour la valeur de n
-    } else if (User.nom === "") {
-      setN(1); // Utilisation de setN pour mettre à jour la valeur de n
+    if (User.nom === "" && User.email === "") {
+      setN(4);
+    } else if (User.nom === "" ) {
+      setN(1);
     } else if (User.email === "") {
       setN(2);
-    } else if (User.password === "") {
-      setN(3);
-    } else if (passwordLowerCase.includes(normalizedNom)) {
-      setN(8);
     } else {
       formData.append("email", User.email);
       formData.append("nom", User.nom);
-      formData.append("prenom", User.prenom);
-      formData.append("password", User.password);
-      if (image === undefined) {
-        setN(9);
-      } else {
-        formData.append("image_user", image, `${User.nom}+.png`);
+      if (image !== undefined) {
+        formData.append("image_user", image, `${User.nom}.png`);
+      }
         const res = await updatecentre(formData, User._id, config);
         console.log(res.data);
         if (res.data.message === undefined) {
-          window.location.replace(`/admin/tables/`);
+          window.location.replace(`/profile`);
         } else {
           setmessageerr(res.data.message);
           console.log(res.data.message);
-          // showNotification('error', res.data.message, 'Erreur')
-        }
       }
     }
+    console.log(n)
   };
   return (
     <>
@@ -314,28 +294,7 @@ export default function Dashboard() {
                             )}
                           </div>
                         </div>
-                      ) : message === "centre" ? (
-                        <div className="w-full lg:w-6/12 px-4">
-                          <div className="relative w-full mb-3">
-                            <label
-                              className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                              htmlFor="grid-password"
-                            >
-                              Image
-                            </label>
-                            <input
-                              className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                              placeholder="image_user"
-                              type="file"
-                              name="image_user"
-                              onChange={(e) => handlechangeFile(e)}
-                              label="image_user"
-                              aria-label="image_user"
-                              // value={User.image_user}
-                            />
-                          </div>
-                        </div>
-                      ): null}
+                      ) : null}
 
                       <div className="w-full lg:w-6/12 px-4">
                         <div className="relative w-full mb-3">
