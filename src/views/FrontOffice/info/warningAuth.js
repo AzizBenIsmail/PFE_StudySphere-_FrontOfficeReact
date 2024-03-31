4197f89d151f92a79df78760610089d0e6746050
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-31 03:37:50 +0100

    activation du Button modifier mot de passe

--- src/views/FrontOffice/info/warningAuth.js
+++ src/views/FrontOffice/info/warningAuth.js
@@ -141,7 +141,10 @@ export default function Index () {
                     <p className="mb-4 text-blueGray-500">
                       <button
                         className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                        type="button">
+                        type="button"
+                        onClick={() => {
+                          history.push("/updatePassword");
+                        }} >
                         Modifier
                       </button>
                     </p>
