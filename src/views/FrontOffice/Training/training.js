ede1244b7c1c8f663901171ff2ba480949765697
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-04-03 12:30:50 +0100

    une nouveau désigne pour la liste de tous les formation

--- src/views/FrontOffice/Training/training.js
+++ src/views/FrontOffice/Training/training.js
@@ -6,7 +6,7 @@ import Footer from "../../../components/Footers/Footer.js";
 import Cookies from "js-cookie";
 import { getUserAuth } from "../../../Services/Apiauth";
 import { getAllFormations } from "../../../Services/ApiFormation";
-import { FaChevronRight , FaChevronLeft  } from "react-icons/fa";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 export default function Landing() {
   const [user, setUser] = useState(null);
@@ -51,7 +51,7 @@ export default function Landing() {
       const res = await getAllFormations(config);
       setFormations(res.data.formations);
     } catch (error) {
-      console.error('Error loading formations:', error);
+      console.error("Error loading formations:", error);
     }
   }, [config]);
 
@@ -61,7 +61,10 @@ export default function Landing() {
 
   const indexOfLastFormation = currentPage * formationsPerPage;
   const indexOfFirstFormation = indexOfLastFormation - formationsPerPage;
-  const currentFormations = formations.slice(indexOfFirstFormation, indexOfLastFormation);
+  const currentFormations = formations.slice(
+    indexOfFirstFormation,
+    indexOfLastFormation
+  );
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -99,78 +102,45 @@ export default function Landing() {
         </div>
         <section className="pb-20 bg-blueGray-200 -mt-24">
           <div className="container mx-auto px-4">
-            <div className="flex flex-wrap">
+            <div className="grid grid-cols-1">
               {currentFormations.map((formation) => (
                 <div
-                  className="pt-6 w-full md:w-2/12 px-4 text-center"
+                  className="pt-6 w-full px-4 "
                   key={formation._id}
                 >
-                  <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                    <div className="px-4 py-5 flex-auto">
-                      <div className="hover:-mt-4 mt-1 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                  <div className="relative flex flex-row items-center justify-between min-w-0 break-words bg-centre w-full mb-8 shadow-lg rounded-lg">
+                    <div className="p-4">
+                      <div className="hover:-mt-4 mt-1 relative flex flex-col min-w-0 break-words bg-centre w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
                         <a href={`/DetailsFormation/${formation._id}`}>
                           <img
                             alt="..."
                             className="align-middle border-none max-w-full h-auto rounded-lg"
                             src={`http://localhost:5000/images/Formations/${formation.image_Formation}`}
-                            // style={{ width: "350px", height: "350px" }}
                             style={{ width: "350px", height: "220px" }}
                           />
                         </a>
-                        <span
-                          style={{
-                            position: "absolute",
-                            top: "5%",
-                            left: "82%",
-                            transform: "translate(-50%, -50%) ",
-                          }}
-                        >
-                          <Link to="/landing">
-                            <img
-                              alt="..."
-                              className="shadow rounded-full max-w-full h-auto align-middle border-none bg-indigo-500"
-                              src={`http://localhost:5000/images/Users/${formation.formateur.image_user}`}
-                              style={{ width: "70px" }}
-                            />
-                          </Link>
-                        </span>
-                        <span
-                          style={{
-                            position: "absolute",
-                            top: "94%",
-                            left: "50%",
-                            transform: "translate(-50%, -50%)",
-                          }}
-                        >
-                          <Link to="/landing">
-                            <img
-                              alt="..."
-                              className="shadow rounded-full max-w-full h-auto align-middle border-none bg-indigo-500"
-                              src={`http://localhost:5000/images/Users/${formation.centre.image_user}`}
-                              style={{ width: "70px" }}
-                            />
-                          </Link>
-                        </span>
                       </div>
-                      <div className="flex flex-wrap">
+                    </div>
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="flex pb-6 flex-wrap">
                         {formation.competences
-                        .split(",")
-                        .map((competence, index) => (
-                          <span
-                            key={index}
-                            style={{
-                              border: "2px solid rgba(186, 230, 253, 1)",
-                              marginRight:
-                                index ===
-                                formation.competences.split(",").length - 1
-                                  ? "0"
-                                  : "5px", // Ne pas ajouter de marge à droite pour la dernière compétence
-                            }}
-                            className="text-xs font-semibold mb-2 inline-block py-1 px-2 uppercase rounded-full text-blueGray-600 uppercase last:mr-0 mr-1"
-                          >
+                          .split(",")
+                          .map((competence, index) => (
+                            <span
+                              key={index}
+                              style={{
+                                border: "2px solid rgba(186, 230, 253, 1)",
+                                marginRight:
+                                  index ===
+                                  formation.competences.split(",").length - 1
+                                    ? "0"
+                                    : "5px",
+                              }}
+                              className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-black bg-white uppercase last:mr-0 mr-1"
+                              style={{ border: "1px solid black" }}                            >
                               {competence.trim()}
                             </span>
-                        ))}
+                          ))}
                       </div>
                       <h6 className="text-xl font-semibold">
                         {formation.titre}
@@ -178,7 +148,84 @@ export default function Landing() {
                       <p className="mt-2 mb-4 text-blueGray-500">
                         {formation.description}
                       </p>
+                      <div className="flex items-center mb-4">
+                        <img
+                          alt="..."
+                          className="shadow rounded-full max-w-full h-auto align-middle border-none bg-indigo-500"
+                          src={`http://localhost:5000/images/Users/${formation.centre.image_user}`}
+                          style={{ width: "25px" }}
+                        />
+                        <p className="text-xs text-blueGray-400 ml-1 ">posté par {formation.centre.nom}</p>
+                        <img
+                          alt="..."
+                          className="shadow ml-1 rounded-full max-w-full h-auto align-middle border-none bg-indigo-500"
+                          src={`http://localhost:5000/images/Users/${formation.formateur.image_user}`}
+                          style={{ width: "25px" }}
+                        />
+                        <p className="text-xs text-blueGray-400 ml-1">posté par {formation.formateur.nom}</p>
+                      </div>
+
                     </div>
+                    <div className="px-4 py-5 flex-auto">
+                      <h6 className="text-xl font-semibold">
+                        Date
+                      </h6>
+                      <p className="mt-2 mb-4 text-blueGray-500">
+                        {formation.dateDebut}
+                      </p>
+                    </div>
+                    <div className="px-4 py-5 flex-auto">
+                      <h6 className="text-xl font-semibold">
+                        Date
+                      </h6>
+                      <p className="mt-2 mb-4 text-blueGray-500">
+                        {formation.dateDebut}
+                      </p>
+                    </div>
+                    <div className="px-4 py-5 flex-auto">
+                    <h6 className="text-xl font-semibold">
+                      Date
+                    </h6>
+                    <p className="mt-2 mb-4 text-blueGray-500">
+                      {formation.dateDebut}
+                    </p>
+                  </div>
+                    {/*<div className="p-4">*/}
+                    {/*  <span*/}
+                    {/*    style={{*/}
+                    {/*      position: "absolute",*/}
+                    {/*      top: "5%",*/}
+                    {/*      left: "2%",*/}
+                    {/*      transform: "translate(-50%, -50%) ",*/}
+                    {/*    }}*/}
+                    {/*  >*/}
+                    {/*    <Link to="/landing">*/}
+                    {/*      <img*/}
+                    {/*        alt="..."*/}
+                    {/*        className="shadow rounded-full max-w-full h-auto align-middle border-none bg-indigo-500"*/}
+                    {/*        src={`http://localhost:5000/images/Users/${formation.formateur.image_user}`}*/}
+                    {/*        style={{ width: "70px" }}*/}
+                    {/*      />*/}
+                    {/*    </Link>*/}
+                    {/*  </span>*/}
+                    {/*  <span*/}
+                    {/*    style={{*/}
+                    {/*      position: "absolute",*/}
+                    {/*      top: "94%",*/}
+                    {/*      left: "50%",*/}
+                    {/*      transform: "translate(-50%, -50%)",*/}
+                    {/*    }}*/}
+                    {/*  >*/}
+                    {/*    <Link to="/landing">*/}
+                    {/*      <img*/}
+                    {/*        alt="..."*/}
+                    {/*        className="shadow rounded-full max-w-full h-auto align-middle border-none bg-indigo-500"*/}
+                    {/*        src={`http://localhost:5000/images/Users/${formation.centre.image_user}`}*/}
+                    {/*        style={{ width: "70px" }}*/}
+                    {/*      />*/}
+                    {/*    </Link>*/}
+                    {/*  </span>*/}
+                    {/*</div>*/}
                   </div>
                 </div>
               ))}
@@ -189,7 +236,11 @@ export default function Landing() {
                   <li>
                     <button
                       onClick={() => paginate(1)}
-                      className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${currentPage === 1 ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'}`}
+                      className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
+                        currentPage === 1
+                          ? "bg-lightBlue-500"
+                          : "bg-white text-lightBlue-500"
+                      }`}
                     >
                       <FaChevronLeft />
                       <FaChevronLeft />
@@ -199,7 +250,11 @@ export default function Landing() {
                     <button
                       onClick={() => paginate(currentPage - 1)}
                       disabled={currentPage === 1}
-                      className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${currentPage === 1 ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'}`}
+                      className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
+                        currentPage === 1
+                          ? "bg-lightBlue-500"
+                          : "bg-white text-lightBlue-500"
+                      }`}
                     >
                       <FaChevronLeft />
                     </button>
@@ -208,7 +263,11 @@ export default function Landing() {
                     <li key={number}>
                       <button
                         onClick={() => paginate(number)}
-                        className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${currentPage === number ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'}`}
+                        className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
+                          currentPage === number
+                            ? "bg-lightBlue-500"
+                            : "bg-white text-lightBlue-500"
+                        }`}
                       >
                         {number}
                       </button>
@@ -217,16 +276,33 @@ export default function Landing() {
                   <li>
                     <button
                       onClick={() => paginate(currentPage + 1)}
-                      disabled={currentPage === Math.ceil(formations.length / formationsPerPage)}
-                      className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${currentPage === Math.ceil(formations.length / formationsPerPage) ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'}`}
+                      disabled={
+                        currentPage ===
+                        Math.ceil(formations.length / formationsPerPage)
+                      }
+                      className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
+                        currentPage ===
+                        Math.ceil(formations.length / formationsPerPage)
+                          ? "bg-lightBlue-500"
+                          : "bg-white text-lightBlue-500"
+                      }`}
                     >
                       <FaChevronRight />
                     </button>
                   </li>
                   <li>
                     <button
-                      onClick={() => paginate(Math.ceil(formations.length / formationsPerPage))}
-                      className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${currentPage === Math.ceil(formations.length / formationsPerPage) ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'}`}
+                      onClick={() =>
+                        paginate(
+                          Math.ceil(formations.length / formationsPerPage)
+                        )
+                      }
+                      className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
+                        currentPage ===
+                        Math.ceil(formations.length / formationsPerPage)
+                          ? "bg-lightBlue-500"
+                          : "bg-white text-lightBlue-500"
+                      }`}
                     >
                       <FaChevronRight />
                       <FaChevronRight />
