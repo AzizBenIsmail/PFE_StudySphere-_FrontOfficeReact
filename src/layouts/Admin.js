a9ff0e10548ca2ed863a3bd8a1fc06a1c2083fee
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-06-25 16:19:59 +0100

    Update Admin.js

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -4,9 +4,9 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 // components
 
-import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
-import HeaderStats from "components/Headers/HeaderStats.js";
+// import AdminNavbar from "components/Navbars/AdminNavbar.js";
+// import Sidebar from "components/Sidebar/Sidebar.js";
+// import HeaderStats from "components/Headers/HeaderStats.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 
 // views
@@ -71,11 +71,11 @@ export default function Admin() {
     <>
     {user !== null && (
       <>
-        <Sidebar user={user}/>
+        {/*<Sidebar user={user}/>*/}
       <div className="relative md:ml-60 bg-blueGray-100">
-        <AdminNavbar />
+        {/*<AdminNavbar />*/}
         {/* Header */}
-        <HeaderStats />
+        {/*<HeaderStats />*/}
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
             <Suspense
               fallback={
