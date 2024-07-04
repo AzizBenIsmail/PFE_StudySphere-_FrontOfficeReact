d0ec66db410d801f775045980a9ff23225a6873c
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-07-04 16:00:47 +0100

    régalage path logout

--- src/components/Dropdowns/UserDropdownDashboard.js
+++ src/components/Dropdowns/UserDropdownDashboard.js
@@ -124,7 +124,7 @@ const UserDropdownDashboard = () => {
         {/*</a>*/}
         {/*<div className="h-0 my-2 border border-solid border-blueGray-100"/>*/}
         <a
-          href="#pablo"
+          href="/"
           className={
             'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
           }
--- src/components/Dropdowns/UserDropdownLanding.js
+++ src/components/Dropdowns/UserDropdownLanding.js
@@ -223,7 +223,7 @@ const UserDropdown = ({ user }) => {
         </button>
         <div className="h-0 my-2 border border-solid border-blueGray-100"/>
         <a
-          href="#pablo"
+          href="/"
           className={
             'flex items-center text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
           }
