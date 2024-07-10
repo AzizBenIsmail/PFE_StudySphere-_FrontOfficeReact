ff1dad1d58b6790108186ecc6a99852395eaabe6
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-07-10 16:37:33 +0100

    liste des formation responsive

--- src/views/FrontOffice/Training/training.js
+++ src/views/FrontOffice/Training/training.js
@@ -115,11 +115,6 @@ export default function Landing () {
     })
   }
 
-  const convertMinutesToHours = (minutes) => {
-    const hours = Math.floor(minutes / 60)
-    const remainingMinutes = minutes % 60
-    return `${hours}H${remainingMinutes}Min`
-  }
 
   return (
     <>
@@ -149,7 +144,7 @@ export default function Landing () {
                   <button
                     className="bg-bleu-500 hover:bg-blue-700 text-orange-500 font-bold py-2 px-4 mt-5 rounded transition duration-300 mr-2"
                     onClick={() => setSortOrder('asc')}>
-                     Prix croissant
+                    Prix croissant
                   </button>
                   <button
                     className="bg-bleu-500 hover:bg-blue-700 text-orange-500 font-bold py-2 px-4 rounded transition duration-300"
@@ -167,119 +162,81 @@ export default function Landing () {
                 </div>
               </div>
             </div>
-
           </div>
-          <div className="grid grid-cols-1">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="flex flex-wrap">
             {currentFormations.map((formation) => (
-              <div
-                className="pt-6 w-full px-4 "
-                key={formation._id}
-              >
-                <div
-                  className="relative flex flex-row items-center justify-between min-w-0 break-words bg-centre w-full mb-8 shadow-lg rounded-lg"
-                  onMouseEnter={e => e.currentTarget.style.boxShadow = '0px 0px 30px 0px rgba(0,0,0,0.3)'}
-                  onMouseLeave={e => e.currentTarget.style.boxShadow = 'none'}
-                >
+              <div className="pt-6 md:w-4/12 px-6" key={formation._id}>
+                <div className="relative items-center justify-between min-w-0 break-words bg-white mb-8 shadow-lg rounded-lg">
                   <div className="p-4">
-                    {/*<div className="hover:-mt-4 mt-1 relative flex flex-col min-w-0 break-words bg-centre w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">*/}
-                    <div className="">
-                      <Link to={`/landing/detailscours/${formation._id}`}>
-                        <img
-                          onMouseEnter={e => e.currentTarget.style.boxShadow = '0px 0px 30px 0px rgba(0,0,0,0.3)'}
-                          onMouseLeave={e => e.currentTarget.style.boxShadow = 'none'}
-                          alt="..."
-                          className="align-middle border-none max-w-full h-auto rounded-lg"
-                          src={`${process.env.REACT_APP_API_URL_IMAGE_FORMATIONS}/${formation.image_Formation}`}
-                          style={{ width: '350px', height: '220px' }}
-                        />
-                      </Link>
-                    </div>
+                    <Link to={`/landing/detailscours/${formation._id}`}>
+                      <img
+                        alt="..."
+                        className="align-middle border-none  h-auto rounded-lg"
+                        src={`${process.env.REACT_APP_API_URL_IMAGE_FORMATIONS}/${formation.image_Formation}`}
+                        style={{ width: '350px', height: '220px' }}
+                      />
+                    </Link>
                   </div>
-                  <div className="px-4 py-5 flex-auto">
+                  <div className="px-4 py-5 ">
                     <div className="flex pb-6 flex-wrap">
-                      {formation.competences
-                      .split(',')
-                      .map((competence, index) => (
+                      {formation.competences.split(',').map((competence, index) => (
                         <span
                           key={index}
                           style={{
                             border: '2px solid rgba(226, 232, 240, 1)',
-                            marginRight:
-                              index ===
-                              formation.competences.split(',').length - 1
-                                ? '0'
-                                : '5px',
+                            marginRight: index === formation.competences.split(',').length - 1 ? '0' : '5px',
                           }}
                           className="text-xs font-semibold inline-block text-blueGray-500 py-1 px-2 uppercase rounded-full text-black bg-white uppercase last:mr-0 mr-1"
                         >
-                              {competence.trim()}
-                            </span>
+                    {competence.trim()}
+                  </span>
                       ))}
                     </div>
-                    <h6 className="text-xl font-semibold">
-                      {formation.titre}
-                    </h6>
+                    <h6 className="text-xl font-semibold">{formation.titre}</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">
                       {splitDescription(formation.description, formation._id)}
                     </p>
                     <div className="flex items-center mb-4">
                       <Link to={`/profile/ProfileCenter/${formation.centre._id}`}>
                         <img
-                          onMouseEnter={e => e.currentTarget.style.boxShadow = '0px 0px 30px 0px rgba(0,0,0,0.3)'}
-                          onMouseLeave={e => e.currentTarget.style.boxShadow = 'none'}
                           alt="..."
-                          className="shadow rounded-full max-w-full h-auto align-middle border-none bg-indigo-500"
+                          className="shadow rounded-full  h-auto align-middle border-none bg-indigo-500"
                           src={`${process.env.REACT_APP_API_URL_IMAGE_USERS}/${formation.centre.image_user}`}
                           style={{ width: '25px' }}
                         />
                       </Link>
-                      <p className="text-xs text-blueGray-400 ml-2 ">posté par {formation.centre.nom}</p>
+                      <p className="text-xs text-blueGray-400 ml-2">posté par {formation.centre.nom}</p>
                       <Link to={`/profile/ProfileFormateur/${formation.formateur._id}`}>
                         <img
-                          onMouseEnter={e => e.currentTarget.style.boxShadow = '0px 0px 30px 0px rgba(0,0,0,0.3)'}
-                          onMouseLeave={e => e.currentTarget.style.boxShadow = 'none'}
                           alt="..."
-                          className="shadow ml-2 rounded-full max-w-full h-auto align-middle border-none bg-indigo-500"
+                          className="shadow ml-2 rounded-full  h-auto align-middle border-none bg-indigo-500"
                           src={`${process.env.REACT_APP_API_URL_IMAGE_USERS}/${formation.formateur.image_user}`}
                           style={{ width: '25px' }}
                         />
                       </Link>
                       <p className="text-xs text-blueGray-400 ml-2">posté par {formation.formateur.nom}</p>
                     </div>
-
                   </div>
-                  <div className="px-4 py-5 flex-auto">
-                    <h6 className="text-xl font-semibold">Date</h6>
-                    <div className="flex items-center">
-                      <p className="mt-2 mb-4 text-blueGray-500">
-                        {formatDate(formation.dateDebut)}
-                      </p>
-                      <GiEmptyHourglass style={{ fontSize: '25px' }}/>
-                      <p className="mt-2 mb-4 text-blueGray-500">
-                        {formatTime(formation.dateDebut)}
-                      </p>
+                  <div className="flex flex-wrap justify-between sm:flex-col">
+                    <div className="px-4 flex-auto">
+                      <h6 className="text-xl font-semibold">Prix</h6>
+                      <p className="mt-2 mb-4 text-blueGray-500">{formation.Prix} DT</p>
+                    </div>
+                    <div className="px-4 flex-auto">
+                      <h6 className="text-xl font-semibold">Date</h6>
+                      <div className="flex items-center">
+                        <p className="mt-2 mb-4 text-blueGray-500">{formatDate(formation.dateDebut)}</p>
+                        <GiEmptyHourglass style={{ fontSize: '25px' }} />
+                        <p className="mt-2 mb-4 text-blueGray-500">{formatTime(formation.dateDebut)}</p>
+                      </div>
                     </div>
                   </div>
-                  <div className="px-4 py-5 flex-auto">
-                    <h6 className="text-xl font-semibold">
-                      Duree
-                    </h6>
-                    <p className="mt-2 mb-4 text-blueGray-500">
-                      {convertMinutesToHours(formation.duree)}
 
-                    </p>
-                  </div>
-                  <div className="px-4 py-5 flex-auto">
-                    <h6 className="text-xl font-semibold">
-                      Prix
-                    </h6>
-                    <p className="mt-2 mb-4 text-blueGray-500">
-                      {formation.Prix} DT
-                    </p>
-                  </div>
                 </div>
               </div>
             ))}
+            </div>
           </div>
           <div className="flex justify-center mt-8">
             <nav className="block">
@@ -288,13 +245,11 @@ export default function Landing () {
                   <button
                     onClick={() => paginate(1)}
                     className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
-                      currentPage === 1
-                        ? 'bg-lightBlue-500'
-                        : 'bg-white text-lightBlue-500'
+                      currentPage === 1 ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'
                     }`}
                   >
-                    <FaChevronLeft/>
-                    <FaChevronLeft/>
+                    <FaChevronLeft />
+                    <FaChevronLeft />
                   </button>
                 </li>
                 <li>
@@ -302,12 +257,10 @@ export default function Landing () {
                     onClick={() => paginate(currentPage - 1)}
                     disabled={currentPage === 1}
                     className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
-                      currentPage === 1
-                        ? 'bg-lightBlue-500'
-                        : 'bg-white text-lightBlue-500'
+                      currentPage === 1 ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'
                     }`}
                   >
-                    <FaChevronLeft/>
+                    <FaChevronLeft />
                   </button>
                 </li>
                 {pageNumbers.map((number) => (
@@ -315,9 +268,7 @@ export default function Landing () {
                     <button
                       onClick={() => paginate(number)}
                       className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
-                        currentPage === number
-                          ? 'bg-lightBlue-500'
-                          : 'bg-white text-lightBlue-500'
+                        currentPage === number ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'
                       }`}
                     >
                       {number}
@@ -327,36 +278,23 @@ export default function Landing () {
                 <li>
                   <button
                     onClick={() => paginate(currentPage + 1)}
-                    disabled={
-                      currentPage ===
-                      Math.ceil(formations.length / formationsPerPage)
-                    }
+                    disabled={currentPage === Math.ceil(formations.length / formationsPerPage)}
                     className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
-                      currentPage ===
-                      Math.ceil(formations.length / formationsPerPage)
-                        ? 'bg-lightBlue-500'
-                        : 'bg-white text-lightBlue-500'
+                      currentPage === Math.ceil(formations.length / formationsPerPage) ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'
                     }`}
                   >
-                    <FaChevronRight/>
+                    <FaChevronRight />
                   </button>
                 </li>
                 <li>
                   <button
-                    onClick={() =>
-                      paginate(
-                        Math.ceil(formations.length / formationsPerPage)
-                      )
-                    }
+                    onClick={() => paginate(Math.ceil(formations.length / formationsPerPage))}
                     className={`text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-200 text-white ${
-                      currentPage ===
-                      Math.ceil(formations.length / formationsPerPage)
-                        ? 'bg-lightBlue-500'
-                        : 'bg-white text-lightBlue-500'
+                      currentPage === Math.ceil(formations.length / formationsPerPage) ? 'bg-lightBlue-500' : 'bg-white text-lightBlue-500'
                     }`}
                   >
-                    <FaChevronRight/>
-                    <FaChevronRight/>
+                    <FaChevronRight />
+                    <FaChevronRight />
                   </button>
                 </li>
               </ul>
@@ -364,6 +302,7 @@ export default function Landing () {
           </div>
         </div>
       </section>
+
     </>
   )
 }
