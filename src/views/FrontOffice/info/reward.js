d28c05ab422f12495f82ed9a9c0929984be29ab3
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-07-11 11:39:36 +0100

    update design info

--- src/views/FrontOffice/info/reward.js
+++ src/views/FrontOffice/info/reward.js
@@ -10,6 +10,7 @@ import { getUserAuth } from '../../../Services/Apiauth'
 import Navbar from "../../../components/Navbars/Navbar.js";
 import { SiNintendogamecube } from "react-icons/si";
 import { IoSchoolSharp } from "react-icons/io5";
+import '../../../assets/styles/centeradd.css'
 
 export default function Index() {
 
@@ -18,103 +19,24 @@ export default function Index() {
 
   return (
     <>
-      <div className="container mx-auto py-2">
-        <div className="flex flex-wrap items-center">
-          <div className="w-13/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-32">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-emerald-500">
-              <img
-                alt="..."
-                src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                className="w-full align-middle rounded-t-lg"
-              />
-              <blockquote className="relative p-4 mb-6">
-                <svg
-                  preserveAspectRatio="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 583 95"
-                  className="absolute left-0 w-full block h-95-px -top-94-px"
-                >
-                  <polygon
-                    points="-30,95 583,95 583,65"
-                    className=" text-emerald-500 fill-current"
-                  ></polygon>
-                </svg>
-                <h4 className="text-xl font-bold text-white">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-18 mb-6 shadow-lg rounded-full mr-2 bg-white">
-                    <SiNintendogamecube />
-                  </div>
-                  <span className="inline-block text-center w-full">Félicitations ! Vous avez gagné {xpGagne} XP</span> {/* Ajoutez une classe pour augmenter la largeur */}
-                </h4>
-                <p className="text-md font-light mt-2 text-white">
-                  n'oubliez pas que chaque fois que vous atteignez 1000 points,
-                  vous débloquez une remise spéciale sur nos formations. Alors, plongez-vous dans le processus et
-                  laissez-nous vous guider vers un avenir brillant et rempli de réussite ! 🚀
-                </p>
-              </blockquote>
-            </div>
-          </div>
-
-
-          <div className="w-full md:w-6/12 px-4">
-            <div className="flex flex-wrap">
-              <div className="w-full md:w-6/12 px-4">
-                <div className="relative flex flex-col mt-4">
-                  <div className="px-4 py-5 flex-auto">
-                    <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
-                      <i className="fas fa-sitemap"></i>
-                    </div>
-                    <h6 className="text-xl mb-1 font-semibold">
-                      CSS Components
-                    </h6>
-                    <p className="mb-4 text-blueGray-500">
-                      Notus React comes with a huge number of Fully Coded CSS
-                      components.
-                    </p>
-                  </div>
-                </div>
-                <div className="relative flex flex-col min-w-0">
-                  <div className="px-4 py-5 flex-auto">
-                    <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
-                      <i className="fas fa-drafting-compass"></i>
-                    </div>
-                    <h6 className="text-xl mb-1 font-semibold">
-                      JavaScript Components
-                    </h6>
-                    <p className="mb-4 text-blueGray-500">
-                      We also feature many dynamic components for React,
-                      NextJS, Vue and Angular.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full md:w-6/12 px-4">
-                <div className="relative flex flex-col min-w-0 mt-4">
-                  <div className="px-4 py-5 flex-auto">
-                    <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
-                      <i className="fas fa-newspaper"></i>
-                    </div>
-                    <h6 className="text-xl mb-1 font-semibold">Pages</h6>
-                    <p className="mb-4 text-blueGray-500">
-                      This extension also comes with 3 sample pages. They are
-                      fully coded so you can start working instantly.
-                    </p>
-                  </div>
-                </div>
-                <div className="relative flex flex-col min-w-0">
-                  <div className="px-4 py-5 flex-auto">
-                    <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
-                      <i className="fas fa-file-alt"></i>
-                    </div>
-                    <h6 className="text-xl mb-1 font-semibold">
-                      Documentation
-                    </h6>
-                    <p className="mb-4 text-blueGray-500">
-                      Built by developers for developers. You will love how
-                      easy is to to work with Notus React.
-                    </p>
-                  </div>
-                </div>
-              </div>
+      <div className="features-training-centers-container">
+        <div
+          className="features-training-centers-image-section w-13/12 md:w-6/12 lg:w-4/12 md:px-4 mr-auto ml-auto -mt-32">
+          <img                 src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+                               alt="People"
+               className="relative flex flex-col  break-words bg-white w-full shadow-lg rounded-lg "
+               style={{ width: '550px', height: '520px' }}
+          />
+        </div>
+        <div className="training-features-section">
+          <h2>Félicitations ! </h2>
+          <div className="features-training-centers-feature-item">
+            <div>
+              <h3>Vous avez gagné {xpGagne} XP </h3>
+              <p>
+                <span className="red-text"></span>                   n'oubliez pas que chaque fois que vous atteignez 1000 points,
+                vous débloquez une remise spéciale sur nos formations. Alors, plongez-vous dans le processus et
+                laissez-nous vous guider vers un avenir brillant et rempli de réussite ! 🚀</p>
             </div>
           </div>
         </div>
--- src/views/FrontOffice/info/warningAuth.js
+++ src/views/FrontOffice/info/warningAuth.js
@@ -38,93 +38,6 @@ export default function Index () {
 
   return (
     <>
-      {/*<div className="container mx-auto py-2">*/}
-      {/*  <div className="flex flex-wrap items-center">*/}
-      {/*    <div className="w-13/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-32">*/}
-      {/*      <div*/}
-      {/*        className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-red-500">*/}
-      {/*        <img*/}
-      {/*          alt="..."*/}
-      {/*          src={require('assets/img/securite.jpg').default}*/}
-      {/*          className="w-full align-middle rounded-t-lg"*/}
-      {/*        />*/}
-      {/*        <blockquote className="relative p-4 mb-6">*/}
-      {/*          <svg*/}
-      {/*            preserveAspectRatio="none"*/}
-      {/*            xmlns="http://www.w3.org/2000/svg"*/}
-      {/*            viewBox="0 0 583 95"*/}
-      {/*            className="absolute left-0 w-full block h-95-px -top-94-px"*/}
-      {/*          >*/}
-      {/*            <polygon*/}
-      {/*              points="-30,95 583,95 583,65"*/}
-      {/*              className="text-red-500 fill-current"*/}
-      {/*            ></polygon>*/}
-      {/*          </svg>*/}
-      {/*          <h4 className="text-xl font-bold text-white">*/}
-      {/*            /!*<div*!/*/}
-      {/*            /!*  className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-18 mb-6 shadow-lg rounded-full mr-2 bg-white">*!/*/}
-      {/*            /!*  <SiWelcometothejungle/>*!/*/}
-      {/*            /!*</div>*!/*/}
-      {/*            <span className="inline-block text-center w-full">Alert ! double connexion a été détectée sur votre compte.</span> /!* Ajoutez une classe pour augmenter la largeur *!/*/}
-      {/*          </h4>*/}
-      {/*          <p className="text-md font-light mt-2 text-white">*/}
-      {/*            Alert ! Une nouvelle connexion a été détectée sur votre compte.*/}
-      {/*          </p>*/}
-      {/*        </blockquote>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-
-
-      {/*    <div className="w-full md:w-6/12 px-4">*/}
-      {/*      <div className="flex flex-wrap">*/}
-      {/*        <div className="w-full md:w-6/12 px-4">*/}
-      {/*          <div className="relative flex flex-col mt-4">*/}
-      {/*            <div className="px-4 py-5 flex-auto">*/}
-      {/*              <div*/}
-      {/*                className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">*/}
-      {/*                <AiOutlineFieldNumber style={{ fontSize: '25px' }}*/}
-      {/*                />*/}
-      {/*              </div>*/}
-      {/*              <h6 className="text-xl mb-1 font-semibold">*/}
-      {/*                Numero de connection de Puis la creation du compte*/}
-      {/*              </h6>*/}
-      {/*              {user ? (*/}
-      {/*                <p className="mb-4 text-blueGray-500">*/}
-      {/*                  {user.visitsCount} Tentatives de connexion utilisateur au compte.*/}
-      {/*                </p>*/}
-      {/*              ) : (*/}
-      {/*                <div className="flex justify-center items-center h-screen">*/}
-      {/*                  <InfinitySpin width="200" height="200" color="#4fa94d"/>*/}
-      {/*                </div>*/}
-      {/*              )}*/}
-      {/*            </div>*/}
-      {/*          </div>*/}
-      {/*        </div>*/}
-      {/*        <div className="w-full md:w-6/12 px-4">*/}
-      {/*          <div className="relative flex flex-col min-w-0 mt-4">*/}
-      {/*            <div className="px-4 py-5 flex-auto">*/}
-      {/*              <div*/}
-      {/*                className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">*/}
-      {/*              </div>*/}
-      {/*              <h6 className="text-xl mb-1 font-semibold">Modifier mots de passe</h6>*/}
-      {/*              <p className="mb-4 text-blueGray-500">*/}
-      {/*                <button*/}
-      {/*                  className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"*/}
-      {/*                  type="button"*/}
-      {/*                  onClick={() => {*/}
-      {/*                    window.location.replace(`/AccountManagement/updatePassword`)*/}
-      {/*                  }} >*/}
-      {/*                  Modifier*/}
-      {/*                </button>*/}
-      {/*              </p>*/}
-      {/*            </div>*/}
-      {/*          </div>*/}
-      {/*        </div>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-
       <div className="features-training-centers-container">
         <div
           className="features-training-centers-image-section w-13/12 md:w-6/12 lg:w-4/12 md:px-4 mr-auto ml-auto -mt-32">
@@ -156,27 +69,6 @@ export default function Index () {
             }}>
             Modifier Votre Mot de passe
           </button>
-        </div>
-      </div>
-
-      <div className="course-submission-container">
-        <h2 className="course-submission-title">Course Submission Process</h2>
-        <div className="course-submission-steps">
-          <div className="course-submission-step">
-            <p>1. Create an account with FOR ME to gain access to your own center’s dashboard.</p>
-          </div>
-          <div className="course-submission-step">
-            <p>2. Fill out course title, description, duration, pricing, schedule, and any prerequisites or special
-              requirements.</p>
-          </div>
-          <div className="course-submission-step">
-            <p>3. Enhance your listing with images, videos, or promotional materials to showcase your training center
-              and attract learners.</p>
-          </div>
-          <div className="course-submission-step">
-            <p>4. Once details are filled, submit your course for our team’s review to ensure quality and maximize
-              visibility.</p>
-          </div>
         </div>
       </div>
     </>
